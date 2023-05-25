88a520f611c228b7d07ef632a8225d29023c3e2e
Your Name <you@example.com>
2023-05-25 17:13:16 +0200

    Adding initial matter-most cql schema.

---
+++ src/main/resources/db/changelog/mattermost/create_tables.sh
@@ -0,0 +1,52 @@
+mkdir groups
+
+mkdir users
+
+mkdir channels
+
+mkdir core
+
+mkdir chat
+
+midir migrations
+
+mkdir auth
+
+touch core/auditlog.cql
+touch core/bots.cql
+touch channels/channelmembers.cql
+touch channels/channels.cql
+touch core/commands.cql
+touch core/compliance.cql
+touch chat/customemoji.cql
+touch channels/groupchannels.cql
+touch groups/groupteams.cql
+touch groups/groups.cql
+touch core/incomingwebhooks.cql
+touch core/jobs.cql
+touch core/licenses.cql
+touch auth/oauthaccessdata.cql
+touch auth/oauthapps.cql
+touch auth/oauthauthdata.cql
+touch core/outgoingwebhooks.cql
+touch core/pluginkeyvaluestore.cql
+touch chat/posts.cql
+touch core/preferences.cql
+touch productnoticeviewstate.cql
+touch channels/publicchannels.cql
+touch channels/reactions.cql
+touch core/remoteclusters.cql
+touch core/retentionpolicies.cql
+touch core/retentionpolicieschannels.cql
+touch core/retentionpoliciesteams.cql
+touch users/roles.cql
+touch migrations/db_migrations.cql 
+touch migrations/schemes.cql
+touch auth/sessions.cql
+touch channels/sharedchannelattachments.cql
+touch channels/sharedchannelremotes.cql
+touch channels/sharedchannels.cql
+touch channels/sharedchannelusers.cql
+touch channels/sidebarcategories.cql
+touch channels/sidebarchannels.cql
+touch users/status.cql
